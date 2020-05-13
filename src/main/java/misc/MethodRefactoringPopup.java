990bf86bbd9c8865f552458c9a32d0e30b70d226
ioana <a.i.savu-1@student.tudelft.nl>
2020-05-13 11:53:57 +0200

    Use JBList instead of labels for method history popup.

--- src/main/java/misc/MethodRefactoringPopup.java
+++ src/main/java/misc/MethodRefactoringPopup.java
@@ -5,18 +5,18 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBPanel;
-import com.intellij.util.ui.UIUtil;
 import com.intellij.vcs.log.VcsLogFilterCollection;
 import com.intellij.vcs.log.impl.VcsLogManager;
 import com.intellij.vcs.log.impl.VcsProjectLog;
 import com.intellij.vcs.log.visible.filters.VcsLogFilterObject;
 import data.RefactoringInfo;
-import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.swing.BorderFactory;
 
 
@@ -42,63 +42,38 @@ public class MethodRefactoringPopup {
   public void show(List<RefactoringInfo> refactorings, String methodName, DataContext datacontext) {
 
     JBPanel panel;
-    Font font = new Font(".SFNS-Regular", Font.PLAIN, 13);
-
     if (refactorings == null || refactorings.isEmpty()) {
       panel = new JBPanel(new GridLayout(0, 1));
       panel.add(new JBLabel("No refactorings for this method."));
     } else {
-      panel = new JBPanel(new GridLayout(refactorings.size(), 1));
-      for (RefactoringInfo s : refactorings) {
-        String string = s.getType().getDisplayName();
+      panel = new JBPanel();
+      JBList<String> list = new JBList<>(refactorings.stream()
+          .map(s -> new String(s.getType().getDisplayName() + ":  "
+              + s.getSignatureBefore() + " -> " + s.getSignatureAfter()))
+          .collect(Collectors.toList()));
+      MouseAdapter mouseAdapter = new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+          super.mouseClicked(e);
+          VcsLogFilterCollection filters = VcsLogFilterObject.collection();
+          VcsLogManager.LogWindowKind kind = VcsLogManager.LogWindowKind.TOOL_WINDOW;
+          kind = VcsLogManager.LogWindowKind.TOOL_WINDOW;
+          VcsProjectLog.getInstance(project).openLogTab(filters, kind)
+              .getVcsLog()
+              .jumpToReference(refactorings.get(list.locationToIndex(e.getPoint())).getCommitId());
+        }
+      };
+      list.addMouseListener(mouseAdapter);
+      panel.add(list);
 
-        string += "    " + s.getSignatureBefore() + " -> " + s.getSignatureAfter();
-
-        JBLabel label = new JBLabel(string);
-        label.setFont(font);
-        label.addMouseListener(new MouseListener() {
-          @Override
-          public void mouseClicked(MouseEvent e) {
-            label.setFontColor(UIUtil.FontColor.BRIGHTER);
-            VcsLogFilterCollection filters = VcsLogFilterObject.collection();
-            VcsLogManager.LogWindowKind kind = VcsLogManager.LogWindowKind.TOOL_WINDOW;
-            kind = VcsLogManager.LogWindowKind.TOOL_WINDOW;
-            VcsProjectLog.getInstance(project).openLogTab(filters, kind)
-                .getVcsLog().jumpToReference(s.getCommitId());
-          }
-
-          @Override
-          public void mousePressed(MouseEvent e) {
-
-          }
-
-          @Override
-          public void mouseReleased(MouseEvent e) {
-
-          }
-
-          @Override
-          public void mouseEntered(MouseEvent e) {
-
-          }
-
-          @Override
-          public void mouseExited(MouseEvent e) {
-
-          }
-
-
-        });
-        panel.add(label);
-      }
       panel.setBorder(BorderFactory.createTitledBorder(
           BorderFactory.createEtchedBorder(), "Refactoring history for "
               + methodName.substring(methodName.lastIndexOf(".") + 1)));
     }
 
-
     JBPopup popup = JBPopupFactory.getInstance()
         .createComponentPopupBuilder(panel, null).createPopup();
     popup.showInBestPositionFor(datacontext);
+
   }
 }
