4eee7478ebd4772021f623f62e713d309331b4f6
Matei <anton.matei10@gmail.com>
2020-05-26 14:42:03 +0300

    Add sub-highlighting for AddMethodAnnotationHandler

--- src/main/java/data/types/methods/AddMethodAnnotationHandler.java
+++ src/main/java/data/types/methods/AddMethodAnnotationHandler.java
@@ -15,6 +15,12 @@ public class AddMethodAnnotationHandler extends Handler {
         .setElementBefore(ref.getAnnotation().toString())
         .setElementAfter(null)
         .addMarking(ref.getOperationBefore().codeRange(), ref.getOperationAfter().codeRange())
+        .addMarking(ref.getOperationBefore().codeRange().getStartLine(),
+                ref.getOperationBefore().codeRange().getStartLine() - 1,
+                ref.getAnnotation().getLocationInfo().getStartLine(),
+                ref.getAnnotation().getLocationInfo().getEndLine(),
+                ref.getOperationBefore().codeRange().getFilePath(),
+                ref.getAnnotation().getLocationInfo().getFilePath())
         .setNameBefore(calculateSignature(ref.getOperationBefore()))
         .setNameAfter(calculateSignature(ref.getOperationBefore()));
   }
