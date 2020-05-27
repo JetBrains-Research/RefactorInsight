9f6256ffd1cc14847a3f29d62ace5ea12b3521ef
Matei <anton.matei10@gmail.com>
2020-05-27 12:35:16 +0300

    Add sub-highlighting for ModifyMethodAnnotationHandler

--- src/main/java/data/types/methods/ModifyMethodAnnotationHandler.java
+++ src/main/java/data/types/methods/ModifyMethodAnnotationHandler.java
@@ -3,6 +3,7 @@ package data.types.methods;
 import data.Group;
 import data.RefactoringInfo;
 import data.types.Handler;
+import gr.uom.java.xmi.UMLAnnotation;
 import gr.uom.java.xmi.diff.ModifyMethodAnnotationRefactoring;
 import org.refactoringminer.api.Refactoring;
 
@@ -14,7 +15,9 @@ public class ModifyMethodAnnotationHandler extends Handler {
     return info.setGroup(Group.METHOD)
         .setElementBefore(ref.getAnnotationBefore().toString())
         .setElementAfter(ref.getAnnotationAfter().toString())
-        .addMarking(ref.getAnnotationBefore().codeRange(), ref.getAnnotationAfter().codeRange())
+        .addMarking(ref.getAnnotationBefore().codeRange(), ref.getAnnotationAfter().codeRange(),
+                line -> line.addOffset(ref.getAnnotationBefore().getLocationInfo(),
+                        ref.getAnnotationAfter().getLocationInfo()))
         .setNameBefore(calculateSignature(ref.getOperationBefore()))
         .setNameAfter(calculateSignature(ref.getOperationAfter()));
   }
