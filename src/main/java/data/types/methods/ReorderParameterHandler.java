858ceb038ab5660d91918f4b4fd2f0ce088d8899
Matei <anton.matei10@gmail.com>
2020-06-02 16:15:38 +0300

    Fix checkstyle

--- src/main/java/data/types/methods/ReorderParameterHandler.java
+++ src/main/java/data/types/methods/ReorderParameterHandler.java
@@ -8,17 +8,17 @@ import gr.uom.java.xmi.diff.ReorderParameterRefactoring;
 import org.refactoringminer.api.Refactoring;
 
 public class ReorderParameterHandler extends Handler {
-    @Override
-    public RefactoringInfo specify(Refactoring refactoring, RefactoringInfo info) {
-        ReorderParameterRefactoring ref = (ReorderParameterRefactoring) refactoring;
-        return info.setGroup(Group.METHOD)
-                .setNameBefore(calculateSignature(ref.getOperationBefore()))
-                .setNameAfter(calculateSignature(ref.getOperationAfter()))
-                .setElementBefore(ref.getOperationBefore().toQualifiedString())
-                .setElementAfter(ref.getOperationAfter().toQualifiedString())
-                .addMarking(ref.getOperationBefore().codeRange(), ref.getOperationAfter().codeRange(),
-                        line -> line.addOffset(
-                                ref.getOperationBefore().getLocationInfo(),
-                                ref.getOperationAfter().getLocationInfo()));
-    }
+  @Override
+  public RefactoringInfo specify(Refactoring refactoring, RefactoringInfo info) {
+    ReorderParameterRefactoring ref = (ReorderParameterRefactoring) refactoring;
+    return info.setGroup(Group.METHOD)
+            .setNameBefore(calculateSignature(ref.getOperationBefore()))
+            .setNameAfter(calculateSignature(ref.getOperationAfter()))
+            .setElementBefore(ref.getOperationBefore().toQualifiedString())
+            .setElementAfter(ref.getOperationAfter().toQualifiedString())
+            .addMarking(ref.getOperationBefore().codeRange(), ref.getOperationAfter().codeRange(),
+                line -> line.addOffset(
+                            ref.getOperationBefore().getLocationInfo(),
+                            ref.getOperationAfter().getLocationInfo()));
+  }
 }
