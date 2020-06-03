5c8a62dfde4b2b48ad66545b99779f8be1c83c4a
Bob Brockbernd <b.j.brockbernd@student.tudelft.nl>
2020-06-03 13:12:10 +0200

    Renamed and replaced gitwindow action to follow conventions

--- src/main/java/actions/ToggleRefactoringViewAction.java
+++ src/main/java/actions/ToggleRefactoringViewAction.java
@@ -1,11 +1,11 @@
-package ui;
+package actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import org.jetbrains.annotations.NotNull;
 import services.WindowService;
 
-public class GitWindow extends ToggleAction {
+public class ToggleRefactoringViewAction extends ToggleAction {
 
   @Override
   public void setSelected(@NotNull AnActionEvent e, boolean state) {
