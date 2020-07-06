bb02ce89110247905efbae00bf92c79dd69d63b2
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-07-06 22:12:56 +0300

    Add issue reporter and change vendor and description of the plugin

---
+++ src/main/java/reporter/RefactorInsightErrorReporter.java
@@ -0,0 +1,9 @@
+package reporter;
+
+import com.intellij.diagnostic.ITNReporter;
+
+/**
+ * Provides the opportunity to create and submit issues from IDE in case plugin crashes.
+ */
+public class RefactorInsightErrorReporter extends ITNReporter {
+}
