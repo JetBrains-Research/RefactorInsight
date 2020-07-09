d875bda1e28ebd4db10987ad1415a9bfcde7313f
Ioana <savuandreeaioana@yahoo.com>
2020-07-09 18:30:32 +0300

    Removed unused variables

--- src/main/java/icons/RefactorInsightIcons.java
+++ src/main/java/icons/RefactorInsightIcons.java
@@ -12,9 +12,5 @@ public class RefactorInsightIcons {
 
   public static final @NotNull Icon node = load("/icons/refactorInsightNode.svg");
   public static final @NotNull Icon toggle = load("/icons/refactorInsightToggle.svg");
-  public static final @NotNull Icon toggle_dark = load("/icons/refactorInsightToggle_dark.svg");
   public static final @NotNull Icon toolWindow = load("/icons/refactorInsightToolWindow.svg");
-  public static final @NotNull Icon toolWindow_dark
-      = load("/icons/refactorInsightToolWindow_dark.svg");
-
 }
