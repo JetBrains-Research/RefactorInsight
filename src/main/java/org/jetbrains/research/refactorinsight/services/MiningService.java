0e514f23f2b52cd50d3f834ca79e35cac533149a
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2020-07-22 20:21:33 +0200

    log execution time for initial mining

--- src/main/java/org/jetbrains/research/refactorinsight/services/MiningService.java
+++ src/main/java/org/jetbrains/research/refactorinsight/services/MiningService.java
@@ -1,11 +1,13 @@
 package org.jetbrains.research.refactorinsight.services;
 
+import com.intellij.idea.LoggerFactory;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
@@ -46,6 +48,8 @@ public class MiningService implements PersistentStateComponent<MiningService.MyS
       = new ConcurrentHashMap<String, Set<RefactoringInfo>>();
   private boolean mining = false;
   private MyState innerState = new MyState();
+  
+  private Logger log = Logger.getInstance("RefactorInsight MiningService");
 
   public MiningService() {
   }
@@ -114,13 +118,13 @@ public class MiningService implements PersistentStateComponent<MiningService.MyS
   public void mineRepo(GitRepository repository, int limit) {
     ProgressManager.getInstance()
         .run(new Task.Backgroundable(repository.getProject(), "Mining refactorings", true) {
-
           @Override
           public void onCancel() {
             super.onCancel();
           }
 
           public void run(@NotNull ProgressIndicator progressIndicator) {
+            long startMillis = System.currentTimeMillis();
             mining = true;
             progressIndicator.setText(RefactoringsBundle.message("mining"));
             progressIndicator.setIndeterminate(false);
@@ -155,6 +159,8 @@ public class MiningService implements PersistentStateComponent<MiningService.MyS
               computeRefactoringHistory(repository.getCurrentRevision(), repository.getProject());
             }
             progressIndicator.setText(RefactoringsBundle.message("finished"));
+            long nowMillis = System.currentTimeMillis();
+            log.warn(String.format("Mined refactorings in %s commits in %s ms", limit, nowMillis - startMillis));
           }
         });
   }
