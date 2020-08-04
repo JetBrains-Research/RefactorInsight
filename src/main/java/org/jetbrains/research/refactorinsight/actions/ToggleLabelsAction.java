86eb0b3cf007f9bad41858897c702406fd64d395
Bob Brockbernd <b.j.brockbernd@student.tudelft.nl>
2020-08-04 12:34:11 +0200

    #9 Changed position of label toggle, changed toggleAction to dumbaware.

--- src/main/java/org/jetbrains/research/refactorinsight/actions/ToggleLabelsAction.java
+++ src/main/java/org/jetbrains/research/refactorinsight/actions/ToggleLabelsAction.java
@@ -1,14 +1,14 @@
 package org.jetbrains.research.refactorinsight.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.project.DumbAwareToggleAction;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.research.refactorinsight.services.WindowService;
 
 /**
  * Toggle labeling of commits containing refactorings in VCSTable.
  */
-public class ToggleLabelsAction extends ToggleAction {
+public class ToggleLabelsAction extends DumbAwareToggleAction {
 
   @Override
   public boolean isSelected(@NotNull AnActionEvent e) {
