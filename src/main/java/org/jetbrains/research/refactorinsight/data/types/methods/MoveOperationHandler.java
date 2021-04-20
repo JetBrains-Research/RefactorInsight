d89d918e06c21fa0e74061a9dcdf6be58110e16f
Roman Venediktov <e2e4b6b7@gmail.com>
2021-04-20 03:08:40 +0300

    Add changes detection to MoveOperationHandler

--- src/main/java/org/jetbrains/research/refactorinsight/data/types/methods/MoveOperationHandler.java
+++ src/main/java/org/jetbrains/research/refactorinsight/data/types/methods/MoveOperationHandler.java
@@ -1,5 +1,6 @@
 package org.jetbrains.research.refactorinsight.data.types.methods;
 
+import gr.uom.java.xmi.decomposition.AbstractStatement;
 import gr.uom.java.xmi.diff.MoveOperationRefactoring;
 import org.jetbrains.research.refactorinsight.adapters.CodeRange;
 import org.jetbrains.research.refactorinsight.data.Group;
@@ -9,11 +10,25 @@ import org.jetbrains.research.refactorinsight.data.types.Handler;
 import org.jetbrains.research.refactorinsight.utils.StringUtils;
 import org.refactoringminer.api.Refactoring;
 
+import java.util.List;
+
 public class MoveOperationHandler extends Handler {
 
   @Override
   public RefactoringInfo specify(Refactoring refactoring, RefactoringInfo info) {
     final MoveOperationRefactoring ref = (MoveOperationRefactoring) refactoring;
+
+    List<AbstractStatement> statementsBefore =
+        ref.getOriginalOperation().getBody().getCompositeStatement().getStatements();
+    List<AbstractStatement> statementsAfter =
+        ref.getOriginalOperation().getBody().getCompositeStatement().getStatements();
+    if (statementsBefore.size() == statementsAfter.size()) {
+      boolean equal = true;
+      for (int i = 0; i < statementsBefore.size() && equal; i++) {
+        equal = statementsBefore.get(i).equalFragment(statementsAfter.get(i));
+      }
+      info.setChanged(!equal);
+    }
 
     String classBefore = ref.getOriginalOperation().getClassName();
     String classAfter = ref.getMovedOperation().getClassName();
@@ -40,6 +55,18 @@ public class MoveOperationHandler extends Handler {
                                  RefactoringInfo info) {
     final org.jetbrains.research.kotlinrminer.diff.refactoring.MoveOperationRefactoring ref =
         (org.jetbrains.research.kotlinrminer.diff.refactoring.MoveOperationRefactoring) refactoring;
+
+    List<org.jetbrains.research.kotlinrminer.decomposition.AbstractStatement> statementsBefore =
+        ref.getOriginalOperation().getBody().getCompositeStatement().getStatements();
+    List<org.jetbrains.research.kotlinrminer.decomposition.AbstractStatement> statementsAfter =
+        ref.getOriginalOperation().getBody().getCompositeStatement().getStatements();
+    if (statementsBefore.size() == statementsAfter.size()) {
+      boolean equal = true;
+      for (int i = 0; i < statementsBefore.size() && equal; i++) {
+        equal = statementsBefore.get(i).equalFragment(statementsAfter.get(i));
+      }
+      info.setChanged(!equal);
+    }
 
     String classBefore = ref.getOriginalOperation().getClassName();
     String classAfter = ref.getMovedOperation().getClassName();
