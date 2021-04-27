c3d5d0f37603cf1e4a1e4fe29ebc899d187620f3
Roman Venediktov <e2e4b6b7@gmail.com>
2021-04-27 23:41:27 +0300

    Reorder methods in PsiUtils

--- src/main/java/org/jetbrains/research/refactorinsight/utils/PsiUtils.java
+++ src/main/java/org/jetbrains/research/refactorinsight/utils/PsiUtils.java
@@ -20,12 +20,59 @@ public class PsiUtils {
   }
 
   @Nullable
+  public static PsiClass findClass(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
+    if (psiFile.getLanguage().is(JavaLanguage.INSTANCE)) {
+      return findClassJava(psiFile, qualifiedName);
+    } else {
+      return null;
+    }
+  }
+
+  @Nullable
   public static PsiMethod findMethod(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
     if (psiFile.getLanguage().is(JavaLanguage.INSTANCE)) {
       return findMethodJava(psiFile, qualifiedName);
     } else {
       return null;
     }
+  }
+
+  @Nullable
+  public static PsiClass findClassJava(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
+    PsiElementProcessor.FindElement<PsiElement> processor = new PsiElementProcessor.FindElement<>() {
+      @Override
+      public boolean execute(@NotNull PsiElement each) {
+        if (each instanceof PsiClass) {
+          String eachQualifiedName = ((PsiClass) each).getQualifiedName();
+          if (qualifiedName.equals(eachQualifiedName)) {
+            return setFound(each);
+          }
+        }
+        return true;
+      }
+    };
+    PsiTreeUtil.processElements(psiFile, processor);
+    return (PsiClass) processor.getFoundElement();
+  }
+
+  @Nullable
+  public static PsiMethod findMethodJava(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
+    PsiClass psiClass = findClassJava(psiFile, methodClassName(qualifiedName));
+    if (psiClass == null) {
+      return null;
+    }
+    PsiMethod[] psiMethods = psiClass.findMethodsByName(methodSimpleName(qualifiedName), false);
+    if (psiMethods.length == 0) {
+      throw new AssertionError("Can't find method by name");
+    }
+    String[] searchedMethodSignature = methodSignature(qualifiedName);
+    Optional<PsiMethod> foundMethod = Arrays.stream(psiMethods)
+        .filter(psiMethod -> Arrays.equals(searchedMethodSignature, methodSignature(psiMethod)))
+        .findAny();
+    if (foundMethod.isEmpty()) {
+      throw new AssertionError("Can't find method by type");
+    }
+    return foundMethod.get();
   }
 
   @NotNull
@@ -53,52 +100,5 @@ public class PsiUtils {
   @NotNull
   private static String methodSimpleName(@NotNull String qualifiedName) {
     return qualifiedName.substring(qualifiedName.lastIndexOf('.') + 1, qualifiedName.indexOf('('));
-  }
-
-  @Nullable
-  public static PsiMethod findMethodJava(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
-    PsiClass psiClass = findClassJava(psiFile, methodClassName(qualifiedName));
-    if (psiClass == null) {
-      return null;
-    }
-    PsiMethod[] psiMethods = psiClass.findMethodsByName(methodSimpleName(qualifiedName), false);
-    if (psiMethods.length == 0) {
-      throw new AssertionError("Can't find method by name");
-    }
-    String[] searchedMethodSignature = methodSignature(qualifiedName);
-    Optional<PsiMethod> foundMethod = Arrays.stream(psiMethods)
-        .filter(psiMethod -> Arrays.equals(searchedMethodSignature, methodSignature(psiMethod)))
-        .findAny();
-    if (foundMethod.isEmpty()) {
-      throw new AssertionError("Can't find method by type");
-    }
-    return foundMethod.get();
-  }
-
-  @Nullable
-  public static PsiClass findClass(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
-    if (psiFile.getLanguage().is(JavaLanguage.INSTANCE)) {
-      return findClassJava(psiFile, qualifiedName);
-    } else {
-      return null;
-    }
-  }
-
-  @Nullable
-  public static PsiClass findClassJava(@NotNull PsiFile psiFile, @NotNull String qualifiedName) {
-    PsiElementProcessor.FindElement<PsiElement> processor = new PsiElementProcessor.FindElement<>() {
-      @Override
-      public boolean execute(@NotNull PsiElement each) {
-        if (each instanceof PsiClass) {
-          String eachQualifiedName = ((PsiClass) each).getQualifiedName();
-          if (qualifiedName.equals(eachQualifiedName)) {
-            return setFound(each);
-          }
-        }
-        return true;
-      }
-    };
-    PsiTreeUtil.processElements(psiFile, processor);
-    return (PsiClass) processor.getFoundElement();
   }
 }
