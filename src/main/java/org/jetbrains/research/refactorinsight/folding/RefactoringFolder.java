5229fab3d4fbe44c7582cbe687b8ff9ff620246d
Roman Venediktov <e2e4b6b7@gmail.com>
2021-05-11 17:49:25 +0300

    Fix Nullability in RefactoringFolder

--- src/main/java/org/jetbrains/research/refactorinsight/folding/RefactoringFolder.java
+++ src/main/java/org/jetbrains/research/refactorinsight/folding/RefactoringFolder.java
@@ -7,12 +7,14 @@ import com.intellij.diff.FrameDiffTool;
 import com.intellij.diff.contents.DiffContent;
 import com.intellij.diff.requests.DiffRequest;
 import com.intellij.diff.requests.SimpleDiffRequest;
+import com.intellij.diff.tools.util.base.DiffViewerBase;
 import com.intellij.diff.tools.util.side.OnesideTextDiffViewer;
 import com.intellij.diff.tools.util.side.ThreesideTextDiffViewer;
 import com.intellij.diff.tools.util.side.TwosideTextDiffViewer;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.FoldRegion;
 import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
@@ -21,6 +23,7 @@ import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.research.refactorinsight.adapters.RefactoringType;
+import org.jetbrains.research.refactorinsight.data.RefactoringEntry;
 import org.jetbrains.research.refactorinsight.data.RefactoringInfo;
 import org.jetbrains.research.refactorinsight.folding.handlers.ExtractOperationFoldingHandler;
 import org.jetbrains.research.refactorinsight.folding.handlers.FoldingHandler;
@@ -59,64 +62,75 @@ public class RefactoringFolder {
    * @param request Associated diff request.
    */
   public static void foldRefactorings(@NotNull FrameDiffTool.DiffViewer viewer, @NotNull DiffRequest request) {
-    if (request instanceof SimpleDiffRequest) {
-      SimpleDiffRequest diffRequest = (SimpleDiffRequest) request;
-      String commitId = getRevisionAfter(diffRequest);
-      if (commitId != null) {
-        if (viewer instanceof OnesideTextDiffViewer) {
-          RefactoringFolder.foldRefactorings((OnesideTextDiffViewer) viewer, commitId);
-        } else if (viewer instanceof TwosideTextDiffViewer) {
-          RefactoringFolder.foldRefactorings((TwosideTextDiffViewer) viewer, commitId);
-        } else if (viewer instanceof ThreesideTextDiffViewer) {
-          RefactoringFolder.foldRefactorings((ThreesideTextDiffViewer) viewer, commitId);
-        }
-      }
+    if (!(request instanceof SimpleDiffRequest && viewer instanceof DiffViewerBase)) {
+      return;
+    }
+    SimpleDiffRequest diffRequest = (SimpleDiffRequest) request;
+    DiffViewerBase viewerBase = (DiffViewerBase) viewer;
+
+    Project project = viewerBase.getProject();
+    if (project == null) {
+      return;
+    }
+
+    String commitId = getRevisionAfter(diffRequest);
+    if (commitId == null) {
+      return;
+    }
+
+    RefactoringEntry entry = MiningService.getInstance(project).get(commitId);
+    if (entry == null) {
+      return;
+    }
+
+    List<RefactoringInfo> foldableRefactorings =
+        entry.getRefactorings().stream()
+            .filter(info -> foldingHandlers.containsKey(info.getType()))
+            .collect(Collectors.toList());
+
+    if (viewerBase instanceof OnesideTextDiffViewer) {
+      RefactoringFolder.foldRefactorings(foldableRefactorings, (OnesideTextDiffViewer) viewerBase, project);
+    } else if (viewerBase instanceof TwosideTextDiffViewer) {
+      RefactoringFolder.foldRefactorings(foldableRefactorings, (TwosideTextDiffViewer) viewerBase, project);
+    } else if (viewerBase instanceof ThreesideTextDiffViewer) {
+      RefactoringFolder.foldRefactorings(foldableRefactorings, (ThreesideTextDiffViewer) viewerBase, project);
     }
   }
 
   /**
    * Fold only in added files.
    */
-  private static void foldRefactorings(@NotNull OnesideTextDiffViewer viewer, String commitId) {
-    List<RefactoringInfo> foldableRefactorings =
-        MiningService.getInstance(viewer.getProject()).get(commitId).getRefactorings().stream()
-            .filter(info -> foldingHandlers.containsKey(info.getType()))
-            .collect(Collectors.toList());
-
-    Editor editor = viewer.getEditor();
-
-    modifyEditor(editor, foldableRefactorings, false);
+  private static void foldRefactorings(@NotNull List<RefactoringInfo> foldableRefactorings,
+                                       @NotNull OnesideTextDiffViewer viewer,
+                                       @NotNull Project project) {
+    modifyEditor(viewer.getEditor(), foldableRefactorings, project, false);
   }
 
-  private static void foldRefactorings(@NotNull TwosideTextDiffViewer viewer, String commitId) {
-    List<RefactoringInfo> foldableRefactorings =
-        MiningService.getInstance(viewer.getProject()).get(commitId).getRefactorings().stream()
-            .filter(info -> foldingHandlers.containsKey(info.getType()))
-            .collect(Collectors.toList());
-
-    modifyEditor(viewer.getEditor1(), foldableRefactorings, true);
-    modifyEditor(viewer.getEditor2(), foldableRefactorings, false);
+  private static void foldRefactorings(@NotNull List<RefactoringInfo> foldableRefactorings,
+                                       @NotNull TwosideTextDiffViewer viewer,
+                                       @NotNull Project project) {
+    modifyEditor(viewer.getEditor1(), foldableRefactorings, project, true);
+    modifyEditor(viewer.getEditor2(), foldableRefactorings, project, false);
   }
 
-  private static void foldRefactorings(@NotNull ThreesideTextDiffViewer viewer, String commitId) {
-    List<RefactoringInfo> foldableRefactorings =
-        MiningService.getInstance(viewer.getProject()).get(commitId).getRefactorings().stream()
-            .filter(info -> foldingHandlers.containsKey(info.getType()))
-            .collect(Collectors.toList());
+  private static void foldRefactorings(@NotNull List<RefactoringInfo> foldableRefactorings,
+                                       @NotNull ThreesideTextDiffViewer viewer,
+                                       @NotNull Project project) {
+    List<? extends EditorEx> editors = viewer.getEditors();
 
-    List<? extends EditorEx> editor = viewer.getEditors();
-
-    modifyEditor(editor.get(0), foldableRefactorings, true);
-    modifyEditor(editor.get(1), foldableRefactorings, false);
-    modifyEditor(editor.get(2), foldableRefactorings, true);
+    modifyEditor(editors.get(0), foldableRefactorings, project, true);
+    modifyEditor(editors.get(1), foldableRefactorings, project, false);
+    modifyEditor(editors.get(2), foldableRefactorings, project, true);
   }
 
   private static void modifyEditor(@NotNull Editor editor,
                                    @NotNull List<RefactoringInfo> foldableRefactorings,
+                                   @NotNull Project project,
                                    boolean before) {
-    PsiFile psiFile = PsiDocumentManager
-        .getInstance(editor.getProject())
-        .getPsiFile(editor.getDocument());
+    PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
+    if (psiFile == null) {
+      return;
+    }
 
     List<Folding> folds = foldableRefactorings.stream()
         .flatMap(info ->
