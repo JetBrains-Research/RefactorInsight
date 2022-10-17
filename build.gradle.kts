ae87b0ad5b308e993627e64a7dfa933d249500c6
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2022-10-17 18:19:56 +0200

    Update commons-text dependency to 1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.2.1.201812262042-r")
     implementation("org.slf4j:slf4j-log4j12:1.7.7")
     implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.16.0")
-    implementation("org.apache.commons:commons-text:1.6")
+    implementation("org.apache.commons:commons-text:1.10.0")
     implementation("org.kohsuke:github-api:1.95")
     implementation(group = "com.github.tsantalis", name = "refactoring-miner", version = "2.0")
     testImplementation("junit:junit:4.13")
