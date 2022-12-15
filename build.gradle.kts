0642ac62b1ac8ff8b34a952c0116466b9e790d7d
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2022-12-15 18:59:23 +0100

    Add compatibility with IDEA 2022.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.10.1"
 }
 
 group = "org.jetbrains.research.refactorinsight"
-version = "2022.2-1.0"
+version = "2022.3-1.0"
 
 repositories {
     mavenCentral()
@@ -14,17 +14,16 @@ repositories {
 dependencies {
     implementation("com.github.JetBrains-Research:kotlinRMiner:v1.2")
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.2.1.201812262042-r")
-    implementation("org.slf4j:slf4j-log4j12:1.7.7")
     implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.16.0")
     implementation("org.apache.commons:commons-text:1.10.0")
     implementation("org.kohsuke:github-api:1.95")
     implementation(group = "com.github.tsantalis", name = "refactoring-miner", version = "2.0")
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.2")
     testImplementation(group = "org.mockito", name = "mockito-core", version = "3.3.3")
 }
 
 intellij {
-    version.set("2022.2")
-    plugins.set(listOf("com.intellij.java", "git4idea", "github"))
+    version.set("2022.3")
+    plugins.set(listOf("com.intellij.java", "Git4Idea", "org.jetbrains.plugins.github"))
     downloadSources.set(true)
 }
