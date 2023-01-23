cfec334f63a34a9414e5ec7880a4c80fb6c0ba7a
annapotr <potryasaeva.anna@mail.ru>
2023-01-23 21:01:19 +0300

    Added button tool tip text

--- src/main/java/org/jetbrains/research/refactorinsight/ui/DiffHintLineMarkerProvider.java
+++ src/main/java/org/jetbrains/research/refactorinsight/ui/DiffHintLineMarkerProvider.java
@@ -204,6 +204,7 @@ public class DiffHintLineMarkerProvider extends LineMarkerProviderDescriptor {
             });
             button.setSize(26, 24);
             button.setBorder(new JBEmptyBorder(1, 2, 1, 2));
+            button.setToolTipText("Show Diff");
             JLabel text = new JLabel(refactoringInfo.getType().getName());
             JPanel panel = new JPanel();
             panel.add(text);
