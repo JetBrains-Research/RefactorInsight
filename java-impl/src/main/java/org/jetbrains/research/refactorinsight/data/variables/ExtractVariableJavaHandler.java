1fe11cb282c240b7c4b6a6f368f986c6ccbb16ed
Anna Potriasaeva <anchouls@Annas-MacBook-Pro.local>
2023-03-05 23:33:32 +0100

    Fixed Extract Variable

--- java-impl/src/main/java/org/jetbrains/research/refactorinsight/data/variables/ExtractVariableJavaHandler.java
+++ java-impl/src/main/java/org/jetbrains/research/refactorinsight/data/variables/ExtractVariableJavaHandler.java
@@ -15,13 +15,19 @@ public class ExtractVariableJavaHandler extends JavaRefactoringHandler {
     public RefactoringInfo specify(Refactoring refactoring, RefactoringInfo info) {
         ExtractVariableRefactoring ref = (ExtractVariableRefactoring) refactoring;
 
+        ref.getSubExpressionMappings().forEach( leafMapping ->
+                info.addMarking(
+                        createCodeRangeFromJava(leafMapping.getFragment1().codeRange()),
+                        createCodeRangeFromJava(leafMapping.getFragment2().codeRange()),
+                        true
+                )
+        );
+
         return info.setGroup(Group.VARIABLE)
                 .setNameBefore(calculateSignatureForVariableDeclarationContainer(ref.getOperationBefore()))
                 .setNameAfter(calculateSignatureForVariableDeclarationContainer(ref.getOperationAfter()))
                 .setElementBefore(ref.getVariableDeclaration().getVariableDeclaration().toQualifiedString())
-                .setElementAfter(null)
-                .addMarking(createCodeRangeFromJava(ref.getOperationBefore().getBody().getCompositeStatement().codeRange()),
-                        createCodeRangeFromJava(ref.getExtractedVariableDeclarationCodeRange()), true);
+                .setElementAfter(null);
     }
 
 
